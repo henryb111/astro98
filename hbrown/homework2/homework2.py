5d2b154c51b89dc161d4c410bd38fa878f812dc5
Henry <henrybrown@berkeley.edu>
2026-02-04 22:41:23 -0800

    done with hw2

---
+++ hbrown/homework2/homework2.py
@@ -0,0 +1,49 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+#git is a software, github is a website, gitbash is a terminal
+# 2) What’s the difference between the terminal and the command line?
+#terminal is  git bash and command line is a windows thing
+# 3) How does Windows PowerShell differ from Git Bash?
+#windows powershell uses different commands
+# 4) What’s the difference between Anaconda, conda, and Python?
+#anaconda is a python distribution platform, conda is a management system, python is a programming language
+# 5) What is VS Code? 
+#a programming environment
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+#a programming environment online, jupyter lab is a UI
+# 7) What does ~/ mean?
+# home directory
+# 8) What’s the difference between an absolute path and a relative path?
+# absolute comes from home dir and relative path comes from the folder you are in
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+# ~/desktop/astro98/hbrown  vs  ../../hbrown
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+# cd ../
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+# delete the directory
+# 12) What do the following commands do?
+# git add
+# git commit
+# git push
+# git add adds new files to git, git commit commits them and push puts them on the website
+# 13) What's the difference between "git add ." and "git add <file>"?
+# one adds all of the files and the other just a specific file
+# 14) What do "git status" and "git log -1" do?
+# git status prints the status, git log -1 displays the commits
+# 15) What’s the difference between cloning a repository and pulling from it?
+# one takes files from the repository and the other does the entire repository
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+# made a git directories had to delete hidden .git files so I could open them in the website
+# 17) What’s a question you still have? What’s something you’re confused about?
+# Why is this easier than using file explorer
+# 18) Tell me a fun fact!
+# groundhog day groundhog predicted 6 more weeks of winter
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+print(1+1) #prints 2
